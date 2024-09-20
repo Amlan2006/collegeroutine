fa32318113ebb689f9904152259604dc1c8c4fb5
Amlan2006 <101693580+Amlan2006@users.noreply.github.com>
2024-09-20 08:41:50 +0530

    college routine
    version 1 only 1AX

---
+++ script.js
@@ -0,0 +1,166 @@
+const AIML1AX={
+    monday1:{
+        period:"HU101",
+        room:516
+    },
+    monday2:{
+        period:"BREAK",
+        room:" BREAK"
+    },
+    monday3:{
+        period:"CH(AM)191",
+        room:415
+    },
+    monday4:{
+        period:"HU102",
+        room:503
+    },
+    monday5:{
+        period:"HU103",
+        room:503
+    },
+    monday6:{
+        period:"CH(AM)101",
+        room:425
+    },
+    tuesday1:{
+        period:"HU101",
+        room:516
+    },
+    tuesday2:{
+        period:"CS(AM)101",
+        room:516
+    },
+    tuesday3:{
+        period:"BREAK",
+        room:"BREAK"
+    },
+    tuesday4:{
+        period:"M101",
+        room:517
+
+    },
+    tuesday5:{
+        period:"HU191",
+        room:405
+    },
+    tuesday6:{
+        period:"Workshop",
+        room:118
+    },
+    wednesday1:{
+        period:"CS191",
+        room:316
+    },
+    thursday1:{
+        period:"CH(AM)101",
+        room:515
+    },
+    thursday2:{
+        period:"CS(AM)101",
+        room:516
+    },
+    thursday3:{
+        period:"M101",
+        room:516
+    },
+    friday1:{
+        period:"CS(AM)101",
+        room:515
+    },
+    friday2:{
+        period:"M101",
+        room:516
+    },
+    friday3:{
+        period:"BREAK",
+        room:"BREAK"
+    },
+    friday4:{
+        period:"M101(T)",
+        room:220
+    },
+    friday5:{
+        period:"HU101(T)",
+        room:406
+    },
+    friday6:{
+        period:"HU102",
+        room:503
+    }
+
+
+    
+
+
+}
+function tex1MAX(){
+    document.getElementById('tex1MAX').innerHTML="Class is at " + AIML1AX.monday1.room +" of "+ AIML1AX.monday1.period;
+}
+function text2MAX(){
+    document.getElementById('text2MAX').innerHTML="Class is at " + AIML1AX.monday2.room +" of "+ AIML1AX.monday2.period;
+}
+function text3MAX(){
+    document.getElementById('text3MAX').innerHTML="Class is at " + AIML1AX.monday3.room +" of "+ AIML1AX.monday3.period;
+}
+function text4MAX(){
+    document.getElementById('text4MAX').innerHTML="Class is at " + AIML1AX.monday3.room +" of "+ AIML1AX.monday3.period;
+}
+function text5MAX(){
+    document.getElementById('text5MAX').innerHTML="Class is at " + AIML1AX.monday4.room +" of "+ AIML1AX.monday4.period;
+}
+function text6MAX(){
+    document.getElementById('text6MAX').innerHTML="Class is at " + AIML1AX.monday5.room +" of "+ AIML1AX.monday5.period;
+}
+function text7MAX(){
+    document.getElementById('text7MAX').innerHTML="Class is at " + AIML1AX.monday6.room +" of "+ AIML1AX.monday6.period;
+}
+function tex1TUAX(){
+    document.getElementById('tex1TUAX').innerHTML="Class is at " + AIML1AX.tuesday1.room +" of "+ AIML1AX.tuesday1.period;
+}
+function text2TUAX(){
+    document.getElementById('text2TUAX').innerHTML="Class is at " + AIML1AX.tuesday2.room +" of "+ AIML1AX.tuesday2.period;
+}
+function text3TUAX(){
+    document.getElementById('text3TUAX').innerHTML="Class is at " + AIML1AX.tuesday3.room +" of "+ AIML1AX.tuesday3.period;
+}
+function text4TUAX(){
+    document.getElementById('text4TUAX').innerHTML="Class is at " + AIML1AX.tuesday4.room +" of "+ AIML1AX.tuesday4.period;
+}
+function text5TUAX(){
+    document.getElementById('text5TUAX').innerHTML="Class is at " + AIML1AX.tuesday5.room +" of "+ AIML1AX.tuesday5.period;
+}
+function text7TUAX(){
+    document.getElementById('text7TUAX').innerHTML="Class is at " + AIML1AX.tuesday6.room +" of "+ AIML1AX.tuesday6.period;
+}
+function tex1WAX(){
+    document.getElementById('tex1WAX').innerHTML="Class is at " + AIML1AX.wednesday1.room +" of "+ AIML1AX.wednesday1.period;
+}
+function tex1THAX(){
+    document.getElementById('tex1THAX').innerHTML="Class is at " + AIML1AX.thursday1.room +" of "+ AIML1AX.thursday1.period;
+}
+function text2THAX(){
+    document.getElementById('text2THAX').innerHTML="Class is at " + AIML1AX.thursday2.room +" of "+ AIML1AX.thursday2.period;
+}
+function text3THAX(){
+    document.getElementById('text3THAX').innerHTML="Class is at " + AIML1AX.thursday3.room +" of "+ AIML1AX.thursday3.period;
+}
+function tex1FAX(){
+    document.getElementById('tex1FAX').innerHTML="Class is at " + AIML1AX.friday1.room +" of "+ AIML1AX.friday1.period;
+}
+function text2FAX(){
+    document.getElementById('text2FAX').innerHTML="Class is at " + AIML1AX.friday2.room +" of "+ AIML1AX.friday2.period;
+}
+function text3FAX(){
+    document.getElementById('text3FAX').innerHTML="Class is at " + AIML1AX.friday3.room +" of "+ AIML1AX.friday3.period;
+}
+
+function text5FAX(){
+    document.getElementById('text5FAX').innerHTML="Class is at " + AIML1AX.friday4.room +" of "+ AIML1AX.friday4.period;
+}
+function text6FAX(){
+    document.getElementById('text6FAX').innerHTML="Class is at " + AIML1AX.friday5.room +" of "+ AIML1AX.friday5.period;
+}
+function text7FAX(){
+    document.getElementById('text7FAX').innerHTML="Class is at " + AIML1AX.friday6.room +" of "+ AIML1AX.friday6.period;
+}
