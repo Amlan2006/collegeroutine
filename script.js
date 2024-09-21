723814b7d9414e138d04f177b4c147bd65c200aa
Amlan2006 <101693580+Amlan2006@users.noreply.github.com>
2024-09-21 16:06:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -87,12 +87,111 @@ const AIML1AX={
     friday6:{
         period:"HU102",
         room:503
+    },
+    friday7:{
+        period:"CH101",
+        room:425
     }
 
+}
+const AIML1AY={
+    monday1:{
+        period:"HU101",
+        room:516
+    },
+    monday2:{
+        period:"BREAK",
+        room:" BREAK"
+    },
+    // monday3:{
+    //     period:"BREAK",
+    //     room:"BREAK"
+    // },
+    monday4:{
+        period:"HU102",
+        room:503
+    },
+    monday5:{
+        period:"HU103",
+        room:503
+    },
+    monday6:{
+        period:"CH(AM)101",
+        room:425
+    },
+    tuesday1:{
+        period:"HU101",
+        room:516
+    },
+    tuesday2:{
+        period:"CS(AM)101",
+        room:516
+    },
+    tuesday3:{
+        period:"BREAK",
+        room:"BREAK"
+    },
+    tuesday4:{
+        period:"M101",
+        room:517
 
-    
-
-
+    },
+    wednesday1:{
+        period:"CH191",
+        room:416
+    },
+    wednesday2:{
+        period:"BREAK",
+        room:"BREAK"
+    },
+    wednesday3:{
+        period:"Workshop",
+        room:118
+    },
+    thursday1:{
+        period:"CH(AM)101",
+        room:515
+    },
+    thursday2:{
+        period:"CS(AM)101",
+        room:516
+    },
+    thursday3:{
+        period:"M101",
+        room:516
+    },
+    thursday4:{
+        period:"CS191",
+        room:"423B"
+    },
+        friday1:{
+        period:"CS(AM)101",
+        room:515
+    },
+    friday2:{
+        period:"M101",
+        room:516
+    },
+    friday3:{
+        period:"HU191",
+        room:405
+    },
+    friday4:{
+        period:"M101(T)",
+        room:220
+    },
+    friday5:{
+        period:"HU101(T)",
+        room:406
+    },
+    friday6:{
+        period:"HU102",
+        room:503
+    },
+    friday7:{
+        period:"CH101",
+        room:425
+    }
 }
 function tex1MAX(){
     document.getElementById('tex1MAX').innerHTML="Class is at " + AIML1AX.monday1.room +" of "+ AIML1AX.monday1.period;
@@ -115,6 +214,26 @@ function text6MAX(){
 function text7MAX(){
     document.getElementById('text7MAX').innerHTML="Class is at " + AIML1AX.monday6.room +" of "+ AIML1AX.monday6.period;
 }
+
+
+function tex1MAY(){
+    document.getElementById('tex1MAY').innerHTML="Class is at " + AIML1AY.monday1.room +" of "+ AIML1AY.monday1.period;
+}
+function text2MAY(){
+    document.getElementById('text2MAY').innerHTML="Class is at " + AIML1AY.monday2.room +" of "+ AIML1AY.monday2.period;
+}
+
+function text5MAY(){
+    document.getElementById('text5MAY').innerHTML="Class is at " + AIML1AY.monday4.room +" of "+ AIML1AY.monday4.period;
+}
+function text6MAY(){
+    document.getElementById('text6MAY').innerHTML="Class is at " + AIML1AY.monday5.room +" of "+ AIML1AY.monday5.period;
+}
+function text7MAY(){
+    document.getElementById('text7MAY').innerHTML="Class is at " + AIML1AY.monday6.room +" of "+ AIML1AY.monday6.period;
+}
+
+
 function tex1TUAX(){
     document.getElementById('tex1TUAX').innerHTML="Class is at " + AIML1AX.tuesday1.room +" of "+ AIML1AX.tuesday1.period;
 }
@@ -133,9 +252,36 @@ function text5TUAX(){
 function text7TUAX(){
     document.getElementById('text7TUAX').innerHTML="Class is at " + AIML1AX.tuesday6.room +" of "+ AIML1AX.tuesday6.period;
 }
+
+
+function tex1TUAY(){
+    document.getElementById('tex1TUAY').innerHTML="Class is at " + AIML1AY.tuesday1.room +" of "+ AIML1AY.tuesday1.period;
+}
+function text2TUAY(){
+    document.getElementById('text2TUAY').innerHTML="Class is at " + AIML1AY.tuesday2.room +" of "+ AIML1AY.tuesday2.period;
+}
+function text3TUAY(){
+    document.getElementById('text3TUAY').innerHTML="Class is at " + AIML1AY.tuesday3.room +" of "+ AIML1AY.tuesday3.period;
+}
+function text4TUAY(){
+    document.getElementById('text4TUAY').innerHTML="Class is at " + AIML1AY.tuesday4.room +" of "+ AIML1AY.tuesday4.period;
+}
+
 function tex1WAX(){
     document.getElementById('tex1WAX').innerHTML="Class is at " + AIML1AX.wednesday1.room +" of "+ AIML1AX.wednesday1.period;
 }
+
+
+function tex1WAY(){
+    document.getElementById('tex1WAY').innerHTML="Class is at " + AIML1AY.wednesday1.room +" of "+ AIML1AY.wednesday1.period;
+}
+function text3WAY(){
+    document.getElementById('text3WAY').innerHTML="Class is at " + AIML1AY.wednesday2.room +" of "+ AIML1AY.wednesday2.period;
+}
+function text4WAY(){
+    document.getElementById('text4WAY').innerHTML="Class is at " + AIML1AY.wednesday3.room +" of "+ AIML1AY.wednesday3.period;
+}
+
 function tex1THAX(){
     document.getElementById('tex1THAX').innerHTML="Class is at " + AIML1AX.thursday1.room +" of "+ AIML1AX.thursday1.period;
 }
@@ -145,6 +291,22 @@ function text2THAX(){
 function text3THAX(){
     document.getElementById('text3THAX').innerHTML="Class is at " + AIML1AX.thursday3.room +" of "+ AIML1AX.thursday3.period;
 }
+
+function tex1THAY(){
+    document.getElementById('tex1THAY').innerHTML="Class is at " + AIML1AY.thursday1.room +" of "+ AIML1AY.thursday1.period;
+}
+function text2THAY(){
+    document.getElementById('text2THAY').innerHTML="Class is at " + AIML1AY.thursday2.room +" of "+ AIML1AY.thursday2.period;
+}
+function text3THAY(){
+    document.getElementById('text3THAY').innerHTML="Class is at " + AIML1AY.thursday3.room +" of "+ AIML1AY.thursday3.period;
+}
+function text4THAY(){
+    document.getElementById('text4THAY').innerHTML="Class is at " + AIML1AY.thursday4.room +" of "+ AIML1AY.thursday4.period;
+}
+
+
+
 function tex1FAX(){
     document.getElementById('tex1FAX').innerHTML="Class is at " + AIML1AX.friday1.room +" of "+ AIML1AX.friday1.period;
 }
@@ -164,3 +326,74 @@ function text6FAX(){
 function text7FAX(){
     document.getElementById('text7FAX').innerHTML="Class is at " + AIML1AX.friday6.room +" of "+ AIML1AX.friday6.period;
 }
+function text8FAX(){
+    document.getElementById('text8FAX').innerHTML="Class is at " + AIML1AX.friday7.room +" of "+ AIML1AX.friday7.period;
+}
+
+
+function tex1FAY(){
+    document.getElementById('tex1FAY').innerHTML="Class is at " + AIML1AY.friday1.room +" of "+ AIML1AY.friday1.period;
+}
+function text2FAY(){
+    document.getElementById('text2FAY').innerHTML="Class is at " + AIML1AY.friday2.room +" of "+ AIML1AY.friday2.period;
+}
+function text3FAY(){
+    document.getElementById('text3FAY').innerHTML="Class is at " + AIML1AY.friday3.room +" of "+ AIML1AY.friday3.period;
+}
+
+function text5FAY(){
+    document.getElementById('text5FAY').innerHTML="Class is at " + AIML1AY.friday4.room +" of "+ AIML1AY.friday4.period;
+}
+function text6FAY(){
+    document.getElementById('text6FAY').innerHTML="Class is at " + AIML1AY.friday5.room +" of "+ AIML1AY.friday5.period;
+}
+function text7FAY(){
+    document.getElementById('text7FAY').innerHTML="Class is at " + AIML1AY.friday6.room +" of "+ AIML1AY.friday6.period;
+}
+function text8FAY(){
+    document.getElementById('text8FAY').innerHTML="Class is at " + AIML1AY.friday7.room +" of "+ AIML1AY.friday7.period;
+}
+
+function close1(){
+    document.getElementById('Monday').style.display='none';
+}
+function close2(){
+    document.getElementById('Tuesday').style.display='none';
+}
+function close3(){
+    document.getElementById('Wednesday').style.display='none';
+}
+function close4(){
+    document.getElementById('Thursday').style.display='none';
+}
+function close5(){
+    document.getElementById('Friday').style.display='none';
+}
+function close6(){
+    document.getElementById('Monday1').style.display='none';
+}
+function close7(){
+    document.getElementById('Tuesday1').style.display='none';
+}
+function close8(){
+    document.getElementById('Wednesday1').style.display='none';
+}
+function close9(){
+    document.getElementById('Thursday1').style.display='none';
+}
+function close10(){
+    document.getElementById('Friday1').style.display='none';
+}
+// function closed1ax(){
+
+//     document.getElementById('aiml1ax').style.display='none'
+
+//     // console.log("click")
+// }
+// function closed1ay(){
+
+//     document.getElementById('aiml1ay').style.display='none'
+
+//     // console.log("click")
+// }
+
